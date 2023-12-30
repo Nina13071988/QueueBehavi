6c027a7116e7c767da50331e97dd64646932d9d9
Leonchikova Nina <leon4iknina@gmail.com>
2023-12-30 23:17:39 +0300

    QUEUEBEHAVI

---
+++ MarketBehaviour.java
@@ -0,0 +1,5 @@
+public interface MarketBehaviour {
+    void placeOrder(Order order);   //Помещение заказа
+    Order takeOrder();              // Взятие заказа
+    boolean hasOrder();             //Проверка наличия заказа
+}
